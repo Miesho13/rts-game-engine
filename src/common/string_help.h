ce6746e42f7d836d261b44cc066cf048986aa01d
miesho <marcin.ryzewskii@gmail.com>
2026-07-09 19:55:15 +0200

    Clean up string_help.h includes and declaration order

--- src/common/string_help.h
+++ src/common/string_help.h
@@ -2,26 +2,24 @@
 #define __STRING_HELP__ 
 
 #include "./result.h"
-
 #include "./type.h"
-using namespace common::type;
 
 #include <string_view>
+#include <string>
 
+using namespace common::type;
 
-using std::string;
 using std::string_view;
-using common::Result;
+using std::string;
 
 namespace common {
     Ok<f32> to_f32(std::string_view s);
-
-    string get_line(string &input);
-    string_view get_line(string_view &input);
     void skip_line(string &input);
+    string get_line(string &input);
+    string_view get_word(string_view &input, c8 seperator);
+    void skip_word(string_view &input, c8 seperator);
     void skip_line(string_view &input);
-    string_view get_word(string_view &input, c8 seperator = ' ');
-    void skip_word(string_view &input, c8 seperator = ' ');
+    string_view get_line(string_view &input);
 }
 
 #endif // __STRING_HELP__ 
