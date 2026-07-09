5664bc8e5f2ebfa211e17c1213fde461c12d0fd1
miesho <marcin.ryzewskii@gmail.com>
2026-07-09 20:04:40 +0200

    Add startup version logging in main using game::version and log_inf

--- src/game/main.cc
+++ src/game/main.cc
@@ -1,9 +1,15 @@
 #include "game.h"
+#include "info.h"
+#include "common/log.h"
 
 using namespace common::type;
 using game::Core;
 
 int main() {
+    log_inf("Game run {}.{}.{}", game::version.major, 
+                                 game::version.minor, 
+                                 game::version.patch);
+
     Core::RetType ret = Core::inst().init();
     if (ret != Core::RetType::OK) { return ret; }
     return Core::inst().run();
